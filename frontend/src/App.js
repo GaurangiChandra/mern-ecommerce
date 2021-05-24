dbf58323f9eeaed49b83693c12ec0632015ce179
Gaurangi Chandra <gaurangi.chandra@gmail.com>
2021-05-24 12:24:06 +0530

    routes added, links changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,31 @@
 // bootstrap required to made the designing easy. Bootstrap needs to be installed using npm command. 
 // using container, to bring the elements in the middle 
 import React from 'react'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap' 
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
-
+import ProductScreen from './screens/ProductScreen'
 
 //every item included needs to be wrapped, hence using the header , footer, container and stuff
 //to wrap the elements.
 const App = () => {
   return (
     <div className="App">
-     
-     <>
+  
+     <Router>
      <Header />
      {/* py is a bootstrap class, which provides padding on the y-axis */}
      <main className="py-3">  
        <Container>
-       <HomeScreen/>
+         {/*setting route*/}
+       <Route path='/' component={HomeScreen} exact /> 
+       <Route path='/product/:id' component={ProductScreen} exact />
        </Container>
       </main>
      <Footer />
-     </>
+     </Router>
 
     </div>
   );
--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,22 +1,24 @@
 import React from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-const Rating = ({ value, text,color}) => {
+const Rating = ({ value, text, color }) => {
   return (
     <div className="rating">
       <span>
-        <i style={{color}}
+        <i
+          style={{ color }}
           className={
             value >= 1
-              ? 'fas fa-star'
+              ? "fas fa-star"
               : value >= 0.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
           }
         ></i>
       </span>
       <span>
-        <i style={{color}}
+        <i
+          style={{ color }}
           className={
             value >= 2
               ? "fas fa-star"
@@ -27,7 +29,8 @@ const Rating = ({ value, text,color}) => {
         ></i>
       </span>
       <span>
-        <i style={{color}}
+        <i
+          style={{ color }}
           className={
             value >= 3
               ? "fas fa-star"
@@ -38,7 +41,8 @@ const Rating = ({ value, text,color}) => {
         ></i>
       </span>
       <span>
-        <i style={{color}}
+        <i
+          style={{ color }}
           className={
             value >= 4
               ? "fas fa-star"
@@ -49,7 +53,8 @@ const Rating = ({ value, text,color}) => {
         ></i>
       </span>
       <span>
-        <i style={{color}}
+        <i
+          style={{ color }}
           className={
             value >= 5
               ? "fas fa-star"
@@ -65,15 +70,13 @@ const Rating = ({ value, text,color}) => {
   );
 };
 
-Rating.defaultProps =
-{
-   color:'#f8e825',
-}
+Rating.defaultProps = {
+  color: "#f8e825",
+};
 
-Rating.propTypes =
-{
-    value:PropTypes.number.isRequired,
-    text:PropTypes.string,
-    color:PropTypes.string,
-}
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string,
+  color: PropTypes.string,
+};
 export default Rating;
