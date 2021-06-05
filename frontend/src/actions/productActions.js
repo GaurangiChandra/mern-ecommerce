ff23b8fbd8c3e51d85cc38b02dd5ba6e1bfa10f6
Gaurangi Chandra <gaurangi.chandra@gmail.com>
2021-06-05 11:25:15 +0530

    brought redux state to home screen(product data)

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,28 +1,29 @@
-import axios from 'axios'
+import axios from "axios";
 
 import {
-    PRODUCT_LIST_REQUEST,
-    PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL,
-  } from '../constants/productConstants';
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstants";
 
-
-//  we use dispatch to dispatch the actions(cases) presents 
-export const listproducts = () => async(dispatch) =>
-//  redux thunk let us add a function within the function
-{
+//  we use dispatch to dispatch the actions(cases) presents
+export const listProducts =
+  () =>
+  async (dispatch) => //  redux thunk let us add a function within the function
+  {
     try {
-        dispatch({type : PRODUCT_LIST_REQUEST})
+      dispatch({ type: PRODUCT_LIST_REQUEST });
 
-        const{data} = await axios.get('/api/products')
+      const { data } = await axios.get("/api/products");
 
-        dispatch({type: PRODUCT_LIST_SUCCESS,
-        payload : data
-        })
+      dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
     } catch (error) {
-        dispatch({
-            type: PRODUCT_LIST_FAIL , 
-            payload:error.response
-        })
+      dispatch({
+        type: PRODUCT_LIST_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
     }
-}
+  };
