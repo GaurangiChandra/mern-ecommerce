af3bc5f7945b2218dbd35bdcd84ce6119f8b900b
Gaurangi Chandra <gaurangi.chandra@gmail.com>
2021-06-09 23:00:17 +0530

    user login,logout reducer and actions. UI of login page, along with username after logging in.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,38 @@
 // Import is used to add everything in the website at once
 // including the container from react-bootstrap
-// bootstrap required to made the designing easy. Bootstrap needs to be installed using npm command. 
-// using container, to bring the elements in the middle 
-import React from 'react'
-import {BrowserRouter as Router,Route} from 'react-router-dom'
-import {Container} from 'react-bootstrap' 
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
-
+// bootstrap required to made the designing easy. Bootstrap needs to be installed using npm command.
+// using container, to bring the elements in the middle
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 //every item included needs to be wrapped, hence using the header , footer, container and stuff
 //to wrap the elements.
 const App = () => {
   return (
     <div className="App">
-  
-     <Router>
-     <Header />
-     {/* py is a bootstrap class, which provides padding on the y-axis */}
-     <main className="py-3">  
-       <Container>
-         {/*setting route*/}
-       <Route path='/' component={HomeScreen} exact /> 
-       <Route path='/product/:id' component={ProductScreen}  />
-       <Route path='/cart/:id?' component={CartScreen}  />
-       </Container>
-      </main>
-     <Footer />
-     </Router>
-
+      <Router>
+        <Header />
+        {/* py is a bootstrap class, which provides padding on the y-axis */}
+        <main className="py-3">
+          <Container>
+            {/*setting route*/}
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/login" component={LoginScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/" component={HomeScreen} exact />
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
